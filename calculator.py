cc61ccea8bfbb955c75b7438c3b50e33747b1f86
rmedi2112 <rmedina9383@gmail.com>
2026-04-13 17:05:13 -0400

    Implement basic calculator operations and logarithm

--- calculator.py
+++ calculator.py
@@ -4,8 +4,25 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
+import math
 def add(a, b): 
-    pass
+    return a + b
+
+def subtract(a,b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
+
+def logarithm(a, b):
+    return math.log(b, a)
+    
+def exponent(a, b):
+    return a ** b
 
 
